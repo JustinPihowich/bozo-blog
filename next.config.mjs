068f824e8e512450444c0401f81f6a28a2676ae9
Justin Pihowich <pihowich@wisc.edu>
2024-09-14 17:41:16 -0500

    Configure Next to support MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withMDX from '@next/mdx';
 
-export default nextConfig;
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+};
+
+const withMDXConfig = withMDX({
+  extension: /\.mdx?$/,
+  options: {
+    providerImportSource: '@mdx-js/react',
+  },
+})(nextConfig);
+
+export default withMDXConfig;
