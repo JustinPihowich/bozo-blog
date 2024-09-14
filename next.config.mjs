bca2e032ef3c0caec824a1958f9576c69c28d5c4
Justin Pihowich <pihowich@wisc.edu>
2024-09-14 17:29:39 -0500

    Revert "Configure Next config to support MDX"
    This reverts commit 0467f29e1190071cfb53d10704a2c54d67efe0c2.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import withMDX from '@next/mdx';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-};
-
-const mdxConfig = withMDX({
-  extension: /\.mdx?$/,
-})({
-  ...nextConfig, 
-});
-
-export default mdxConfig;
+export default nextConfig;
