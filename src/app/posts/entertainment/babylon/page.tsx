ee2e27d9373e953354f90f97fdae4dfa4f5cb165
Justin Pihowich <pihowich@wisc.edu>
2025-03-13 18:25:16 -0500

    Add Babylon post

---
+++ src/app/posts/entertainment/babylon/page.tsx
@@ -0,0 +1,10 @@
+import Babylon from '../../../../markdown/entertainment/babylon.mdx';
+import PageLayout from '../../../../components/PageLayout/PageLayout'
+
+export default function Page() {
+  return (
+    <PageLayout>
+      <Babylon />
+    </PageLayout>
+  );
+}
