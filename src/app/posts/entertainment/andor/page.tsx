12da0c647a24133e981ca500fdd91540a35008d6
Justin Pihowich <pihowich@wisc.edu>
2025-03-13 20:04:22 -0500

    Add Andor post

---
+++ src/app/posts/entertainment/andor/page.tsx
@@ -0,0 +1,10 @@
+import Andor from '../../../../markdown/entertainment/andor.mdx';
+import PageLayout from '../../../../components/PageLayout/PageLayout'
+
+export default function Page() {
+  return (
+    <PageLayout>
+      <Andor />
+    </PageLayout>
+  );
+}
