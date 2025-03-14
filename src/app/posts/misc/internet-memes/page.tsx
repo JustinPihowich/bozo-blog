d93f42cd26d4a38ec52452b76fcc6eb6339a7609
Justin Pihowich <pihowich@wisc.edu>
2025-03-13 20:04:47 -0500

    Add Internet Memes post

---
+++ src/app/posts/misc/internet-memes/page.tsx
@@ -0,0 +1,10 @@
+import InternetMemes from '../../../../markdown/misc/internet-memes.mdx';
+import PageLayout from '../../../../components/PageLayout/PageLayout'
+
+export default function Page() {
+  return (
+    <PageLayout>
+      <InternetMemes />
+    </PageLayout>
+  );
+}
