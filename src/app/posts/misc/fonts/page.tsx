117bcbc56f99661a910981c5ae9304cae16743d0
Justin Pihowich <pihowich@wisc.edu>
2025-03-13 20:04:34 -0500

    Add Fonts post

---
+++ src/app/posts/misc/fonts/page.tsx
@@ -0,0 +1,10 @@
+import Fonts from '../../../../markdown/misc/fonts.mdx';
+import PageLayout from '../../../../components/PageLayout/PageLayout'
+
+export default function Page() {
+  return (
+    <PageLayout>
+      <Fonts />
+    </PageLayout>
+  );
+}
