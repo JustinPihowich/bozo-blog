5a7c6ea9f250bf7f4101a7a832551a77f94848a4
Justin Pihowich <pihowich@wisc.edu>
2025-03-13 20:04:05 -0500

    Add A Window Into Hardcore Gaming post

---
+++ src/app/posts/misc/a-window-into-hardcore-gaming/page.tsx
@@ -0,0 +1,10 @@
+import AWindowIntoHardcoreGaming from '../../../../markdown/misc/a-window-into-hardcore-gaming.mdx';
+import PageLayout from '../../../../components/PageLayout/PageLayout'
+
+export default function Page() {
+  return (
+    <PageLayout>
+      <AWindowIntoHardcoreGaming />
+    </PageLayout>
+  );
+}
